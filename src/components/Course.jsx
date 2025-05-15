2691ee53ddbbe85a79ec8753fcf809b047a4c86a
Huynh <socialnetworksite15@gmail.com>
2025-05-15 15:54:42 +0700

    part2 - 2.4 (2.3 same 2.2)

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Header from "./Header";
 import Content from "./Content";
 
-export default function Course({ course }) {
-  return (
+export default function Course({ courses }) {
+  return courses.map((course) => (
     <>
       <Header name={course.name} />
       <Content parts={course.parts} />
     </>
-  );
+  ));
 }
