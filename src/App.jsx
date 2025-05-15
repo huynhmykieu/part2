8ffb6f123f9c843efbdea3105b47587e86b1bcd4
Huynh <socialnetworksite15@gmail.com>
2025-05-15 19:57:42 +0700

    part2 - 2.6

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+function App() {
+  const [person, setPerson] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const onHandleChangeName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const onHandleSubmitName = (event) => {
+    event.preventDefault();
+    const newNameObj = {
+      name: newName,
+    };
+
+    setPerson([...person, newNameObj]);
+    // setPerson(person.concat(newNameObj));
+
+    setNewName("");
+  };
+
+  return (
+    <>
+      <h2>Phonebook</h2>
+      <form onSubmit={onHandleSubmitName}>
+        <div>
+          name: <input value={newName} onChange={onHandleChangeName} />
+        </div>
+        <button type="submit">add</button>
+      </form>
+      <h2>Numbers</h2>
+      {person.map((p) => (
+        <div>{p.name}</div>
+      ))}
+    </>
+  );
+}
+
+export default App;
